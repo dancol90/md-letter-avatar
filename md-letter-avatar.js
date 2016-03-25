327a12941a64919f6b0bc63d830a839a8d761ad7
Daniele Colanardi <dancol90@gmail.com>
2016-03-25 18:21:33 +0100

    Wrong directive name

--- md-letter-avatar.js
+++ md-letter-avatar.js
@@ -22,7 +22,7 @@ angular.module('mdLetterAvatar', [])
     palette:    ["#E57373", "#F06292", "#BA68C8", "#9575CD", "#7986CB", "#64B5F6", "#4FC3F7", "#4DD0E1", "#4DB6AC", "#81C784", "#AED581", "#DCE775", "#FFF176", "#FFD54F", "#FFB74D", "#FF8A65", "#A1887F", "#E0E0E0", "#90A4AE"],
 })
 
-.directive('ngLetterAvatar', ['defaults', function(defaults) {
+.directive('mdLetterAvatar', ['defaults', function(defaults) {
     return {
         restrict: 'E',
         scope: {
